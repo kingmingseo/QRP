9fc8673ddb5d966a2c012b2a6c2e08661ae8586b
kingmingseo <multi1971@mju.ac.kr>
2026-05-29 11:40:31 +0900

    [FIX] 간단한 수정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,6 @@ import cookieParser from "cookie-parser"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.CORS_ORIGIN)
   app.use(cookieParser())
 
   app.enableCors({
