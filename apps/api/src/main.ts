743e022d9aefed53a26cfedd0cd9baf540e428b3
kingmingseo <multi1971@mju.ac.kr>
2026-05-29 13:41:11 +0900

    cors 문제 해결

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,10 +5,11 @@ import cookieParser from "cookie-parser"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOrigins = process.env.CORS_ORIGIN?.split(",").map((origin) => origin.trim())
   app.use(cookieParser())
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: corsOrigins,
     credentials: true,
   })
   await app.listen(process.env.PORT ?? 4000);
